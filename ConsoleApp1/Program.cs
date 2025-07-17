039eec1cf9bcfe901baf2897156c2419bfd599a9
Kimdaesun123 <poeticus09@naver.com>
2025-07-17 11:48:21 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿//실습.변수 및 캐스팅
+byte a1 = 30;
+short a2 = 1000;
+int a3 = 2000;
+float a4 = 1.23f;
+double a5 = 0.12345678910;
+decimal a6 = 123456789.10m;
+
+Console.WriteLine($"{a1.GetType()} a1: {a1.ToString()}");
+Console.WriteLine($"{a2.GetType()} a2: {a2.ToString()}");
+Console.WriteLine($"{a3.GetType()} a3: {a3.ToString()}");
+Console.WriteLine($"{a4.GetType()} a4: {a4.ToString()}");
+Console.WriteLine($"{a5.GetType()} a5: {a5.ToString()}");
+Console.WriteLine($"{a6.GetType()} a6: {a6.ToString()}");
