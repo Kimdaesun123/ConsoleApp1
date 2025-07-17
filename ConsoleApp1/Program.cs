09af46f7e68cc7cd048ee1323072b644b3e7917d
Kimdaesun123 <poeticus09@naver.com>
2025-07-17 11:53:27 +0900

    실습.변수 및 캐스팅

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ int a3 = 2000;
 float a4 = 1.23f;
 double a5 = 0.12345678910;
 decimal a6 = 123456789.10m;
-
+ 
 Console.WriteLine($"{a1.GetType()} a1: {a1.ToString()}");
 Console.WriteLine($"{a2.GetType()} a2: {a2.ToString()}");
 Console.WriteLine($"{a3.GetType()} a3: {a3.ToString()}");
