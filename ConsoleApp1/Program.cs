1a63baf882535bd07211b192d678cb9da7cb9a22
Kimdaesun123 <poeticus09@naver.com>
2025-07-17 12:20:13 +0900

    //실습.변수 및 캐스팅

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿//실습.변수 및 캐스팅
+﻿
+//실습.변수 및 캐스팅
 byte a1 = 30;
 short a2 = 1000;
 int a3 = 2000;
@@ -6,9 +7,9 @@ float a4 = 1.23f;
 double a5 = 0.12345678910;
 decimal a6 = 123456789.10m;
  
-Console.WriteLine($"{a1.GetType()} a1: {a1.ToString()}");
-Console.WriteLine($"{a2.GetType()} a2: {a2.ToString()}");
-Console.WriteLine($"{a3.GetType()} a3: {a3.ToString()}");
-Console.WriteLine($"{a4.GetType()} a4: {a4.ToString()}");
-Console.WriteLine($"{a5.GetType()} a5: {a5.ToString()}");
-Console.WriteLine($"{a6.GetType()} a6: {a6.ToString()}");
+Console.WriteLine($"{a1.GetType()} a1: {a1}");
+Console.WriteLine($"{a2.GetType()} a2: {a2}");
+Console.WriteLine($"{a3.GetType()} a3: {a3}");
+Console.WriteLine($"{a4.GetType()} a4: {a4}");
+Console.WriteLine($"{a5.GetType()} a5: {a5}");
+Console.WriteLine($"{a6.GetType()} a6: {a6}");
